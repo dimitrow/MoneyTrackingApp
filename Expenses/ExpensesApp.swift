734251eb0b35a6f57515bbe6c057d200c3fbd777
dimitrow <e.dimitrow@gmail.com>
2022-11-20 20:06:13 +0300

    project initial

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Gene Dimitrow on 20.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
